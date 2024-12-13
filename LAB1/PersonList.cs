280530959fa4c71dd3d9fc5d5d7e4dadcaf77e91
ain11 <ain11@W11075>
2024-12-13 12:49:04 +0700

    Добавьте файлы проекта.

---
+++ LAB1/PersonList.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+
+namespace LAB1
+{
+    public class PersonList
+    {
+        private List<Person> people;
+
+        public PersonList()
+        {
+            people = new List<Person>();
+        }
+
+        // Метод для добавления элемента
+        public void Add(Person person)
+        {
+            people.Add(person);
+        }
+
+        // Метод для удаления элемента
+        public bool Remove(Person person)
+        {
+            return people.Remove(person);
+        }
+
+        // Метод для удаления элемента по индексу
+        public bool RemoveAt(int index)
+        {
+            if (index < 0 || index >= people.Count)
+            {
+                return false; // Возвращаем false, если индекс вне диапазона
+            }
+
+            people.RemoveAt(index);
+            return true; // Успешно удален
+        }
+
+        // Метод для поиска элемента по индексу
+        public Person Get(int index)
+        {
+            if (index < 0 || index >= people.Count)
+            {
+               throw new ArgumentOutOfRangeException("Индекс вне диапазона");
+            }
+
+            return people[index];
+        }
+
+        // Метод для получения индекса элемента
+        public int IndexOf(Person person)
+        {
+            return people.IndexOf(person);
+        }
+
+        // Метод для очистки списка
+        public void Clear()
+        {
+            people.Clear();
+        }
+
+        // Метод для получения количества элементов
+        public int Count()
+        {
+            return people.Count;
+        }
+        public void Print()
+        {
+            foreach (var person in people)
+            {
+                Console.WriteLine(person);
+            }
+        }
+    }
+}
---
+++ LAB1/Program.cs
@@ -0,0 +1,106 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace LAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Создаем два списка персон
+            PersonList list1 = new PersonList();
+            PersonList list2 = new PersonList();
+
+            list1.Add(new Person("Атрём", "Кирякин", 26, Sex.Male));
+            list1.Add(new Person("Виктория", "Тайдонова", 22, Sex.Female));
+            list1.Add(new Person("Егор", "Бухаров", 17, Sex.Male));
+
+            list2.Add(new Person("Михаил", "Шлапак", 28, Sex.Male));
+            list2.Add(new Person("Александр", "Ушаков", 33, Sex.Male));
+            list2.Add(new Person("Павел", "Арефьев", 24, Sex.Male));
+
+            // Выводим содержимое каждого списка
+            Console.WriteLine("Список 1:");
+            list1.Print();
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+
+            Console.WriteLine("\nСписок 2:");
+            list2.Print();
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+
+            // Добавляем нового человека в первый список
+            list1.Add(new Person("Мария", "Николаева", 29, Sex.Female));
+            Console.WriteLine("\nПосле добавления нового человека в первый список:");
+            list1.Print();
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+
+            // Копируем второго человека из первого списка во второй
+            Person personCopy = list1.Get(1); //   (второй элемент)
+            list2.Add(personCopy);
+            Console.WriteLine("\nПосле копирования второго человека из первого списка во второй:");
+            Console.WriteLine("\nСписок 1:");
+            list1.Print();
+            Console.WriteLine("\nСписок 2:");
+            list2.Print();
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+
+            // Удаляем второго человека из первого списка
+            list1.Remove(list1.Get(1));
+            Console.WriteLine("\nПосле удаления второго человека из первого списка:");
+            Console.WriteLine("\nСписок 1:");
+            list1.Print();
+            Console.WriteLine("\nСписок 2:");
+            list2.Print();
+            Console.WriteLine("\nНажмите любую клавишу для продолжения...");
+            Console.ReadKey();
+
+            // Очищаем второй список
+            list2.Clear();
+            Console.WriteLine("\nПосле очистки второго списка:");
+            Console.WriteLine("\nСписок 1:");
+            list1.Print();
+            Console.WriteLine("\nСписок 2 (очищен):");
+            list2.Print();
+            Console.WriteLine("\nНажмите любую клавишу для завершения...");
+            Console.ReadKey();
+
+            PersonList List3 = new PersonList();
+
+            // Пример использования метода ReadFromConsole
+            Console.WriteLine("Введите данные для первого человека:");
+            // создаем временного человека
+            Person person1 = new Person("", "", 0, Sex.Male); 
+            person1.ReadFromConsole();
+            List3.Add(person1);
+
+            // Пример использования метода GetRandomPerson
+            Person randomPerson = Person.GetRandomPerson();
+            List3.Add(randomPerson);
+
+            // Вывод списка людей
+            Console.WriteLine("\nСписок персон:");
+            for (int i = 0; i < List3.Count(); i++)
+            {
+                List3.Get(i).Print(); // используем новый метод Print
+            }
+
+            // Подождите ввода клавиши для завершения
+            Console.WriteLine("\nНажмите любую клавишу для выхода...");
+            Console.ReadKey();
+
+        }
+        
+        
+        
+        
+
+    }
+}
+
+
