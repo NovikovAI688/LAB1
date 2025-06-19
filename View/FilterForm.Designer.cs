d9b7c40db548bac77ac8ed923a4b585886de8e0d
Aleksandr <AleksNov688@gmail.com>
2025-06-19 19:26:39 +0700

    feature: добавлен фильтр

---
+++ View/FilterForm.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace View
+{
+    partial class FilterForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            ElementCheckedListBox = new CheckedListBox();
+            OKbutton = new Button();
+            CancelButton = new Button();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(CancelButton);
+            groupBox1.Controls.Add(OKbutton);
+            groupBox1.Controls.Add(ElementCheckedListBox);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(205, 160);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Filter";
+            // 
+            // ElementCheckedListBox
+            // 
+            ElementCheckedListBox.FormattingEnabled = true;
+            ElementCheckedListBox.Location = new Point(6, 26);
+            ElementCheckedListBox.Name = "ElementCheckedListBox";
+            ElementCheckedListBox.Size = new Size(191, 92);
+            ElementCheckedListBox.TabIndex = 0;
+            ElementCheckedListBox.SelectedIndexChanged += ElementCheckedListBox_SelectedIndexChanged;
+            // 
+            // OKbutton
+            // 
+            OKbutton.Location = new Point(6, 124);
+            OKbutton.Name = "OKbutton";
+            OKbutton.Size = new Size(94, 29);
+            OKbutton.TabIndex = 1;
+            OKbutton.Text = "OK";
+            OKbutton.UseVisualStyleBackColor = true;
+            OKbutton.Click += OKbutton_Click;
+            // 
+            // CancelButton
+            // 
+            CancelButton.Location = new Point(103, 124);
+            CancelButton.Name = "CancelButton";
+            CancelButton.Size = new Size(94, 29);
+            CancelButton.TabIndex = 2;
+            CancelButton.Text = "Cancel";
+            CancelButton.UseVisualStyleBackColor = true;
+            CancelButton.Click += CancelButton_Click;
+            // 
+            // FilterForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(223, 180);
+            Controls.Add(groupBox1);
+            Name = "FilterForm";
+            Text = "Filter";
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button CancelButton;
+        private Button OKbutton;
+        private CheckedListBox ElementCheckedListBox;
+    }
+}
---
+++ View/FilterForm.cs
@@ -0,0 +1,112 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class FilterForm : Form
+    {
+        /// <summary>
+        /// Словарь видов элементов.
+        /// </summary>
+        private readonly Dictionary<string, Type> _elementTypes = new()
+        {
+            {nameof(Resistor), typeof(Resistor)},
+            {nameof(Capacitor), typeof(Capacitor)},
+            {nameof(InductorCoil), typeof(InductorCoil)}
+        };
+
+        /// <summary>
+        /// Словарь названий элементов.
+        /// </summary>
+        private readonly Dictionary<string, string> _listBoxToElementType;
+
+        /// <summary>
+        /// Возвращает или устанавливает свойство поля EventHandler _elementListFiltered.
+        /// </summary>
+        public EventHandler<ElementEventArgsList> ElementListFiltered
+        { get; set; }
+
+        /// <summary>
+        /// Возврат или установка BindingList для MainForm _elementList.
+        /// </summary>
+        public BindingList<PassiveElementBase> ElementList { get; set; }
+
+        /// <summary>
+        /// Конструктор фильтра.
+        /// </summary>
+        public FilterForm()
+        {
+            InitializeComponent();
+
+            _listBoxToElementType = new Dictionary<string, string>()
+            {
+                {"Resistor", nameof(Resistor)},
+                {"Capacitor", nameof(Capacitor)},
+                {"InductorCoil", nameof(InductorCoil)}
+            };
+            ElementCheckedListBox.Items.AddRange
+                (_listBoxToElementType.Keys.ToArray());
+            OKbutton.Enabled = false;
+        }
+        /// <summary>
+        /// Информация для DataGrid.
+        /// </summary>
+        /// <param name="sender">OK.</param>
+        /// <param name="e">Аргумент.</param>
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            var valueFilteredList = new BindingList<PassiveElementBase>();
+            var typeFilteredList = new BindingList<PassiveElementBase>();
+
+            var action = new List<Action<BindingList<PassiveElementBase>>>
+            {
+                typeFilteredList =>
+                {
+                    foreach (var element in ElementList)
+                    {
+                        foreach (var checkedElement in
+                                 ElementCheckedListBox.CheckedItems)
+                        {
+                            if (element.GetType() ==
+                                _elementTypes[_listBoxToElementType
+                                [checkedElement.ToString()]])
+                            {
+
+                                typeFilteredList.Add(element);
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Закрыть.
+        /// </summary>
+        /// <param name="sender">Закрыть.</param>
+        /// <param name="e">Аргумент.</param>
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            var eventArgs = new ElementEventArgsList(ElementList);
+            ElementListFiltered?.Invoke(this, eventArgs);
+        }
+
+        /// <summary>
+        /// Изменение информации в CheckedListBox.
+        /// </summary>
+        /// <param name="sender"> Выбранные элементы.</param>
+        /// <param name="e">Аргументы.</param>
+        private void ElementCheckedListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OKbutton.Enabled = true;
+        }
+    }
+}
