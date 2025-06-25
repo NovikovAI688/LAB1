60990414caf0d4190aab7e328c8f0cfaa7fc0e4e
NovikovAI688 <AleksNov688@gmail.com>
2025-06-25 10:46:56 +0700

    feature(fix): изменение формы фильтра

--- View/FilterForm.Designer.cs
+++ View/FilterForm.Designer.cs
@@ -44,34 +44,38 @@
             groupBox1.Controls.Add(CancelButton);
             groupBox1.Controls.Add(OKbutton);
             groupBox1.Controls.Add(ElementCheckedListBox);
-            groupBox1.Location = new Point(11, 12);
+            groupBox1.Location = new Point(10, 9);
+            groupBox1.Margin = new Padding(3, 2, 3, 2);
             groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(205, 187);
+            groupBox1.Padding = new Padding(3, 2, 3, 2);
+            groupBox1.Size = new Size(179, 140);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Filter";
             // 
             // SearchTextBox
             // 
-            SearchTextBox.Location = new Point(68, 111);
+            SearchTextBox.Location = new Point(60, 83);
+            SearchTextBox.Margin = new Padding(3, 2, 3, 2);
             SearchTextBox.Name = "SearchTextBox";
-            SearchTextBox.Size = new Size(129, 27);
+            SearchTextBox.Size = new Size(113, 23);
             SearchTextBox.TabIndex = 4;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(6, 114);
+            label1.Location = new Point(5, 86);
             label1.Name = "label1";
-            label1.Size = new Size(56, 20);
+            label1.Size = new Size(45, 15);
             label1.TabIndex = 3;
             label1.Text = "Search:";
             // 
             // CancelButton
             // 
-            CancelButton.Location = new Point(103, 152);
+            CancelButton.Location = new Point(90, 114);
+            CancelButton.Margin = new Padding(3, 2, 3, 2);
             CancelButton.Name = "CancelButton";
-            CancelButton.Size = new Size(94, 29);
+            CancelButton.Size = new Size(82, 22);
             CancelButton.TabIndex = 2;
             CancelButton.Text = "Cancel";
             CancelButton.UseVisualStyleBackColor = true;
@@ -79,9 +83,10 @@
             // 
             // OKbutton
             // 
-            OKbutton.Location = new Point(6, 152);
+            OKbutton.Location = new Point(5, 114);
+            OKbutton.Margin = new Padding(3, 2, 3, 2);
             OKbutton.Name = "OKbutton";
-            OKbutton.Size = new Size(94, 29);
+            OKbutton.Size = new Size(82, 22);
             OKbutton.TabIndex = 1;
             OKbutton.Text = "OK";
             OKbutton.UseVisualStyleBackColor = true;
@@ -90,19 +95,21 @@
             // ElementCheckedListBox
             // 
             ElementCheckedListBox.FormattingEnabled = true;
-            ElementCheckedListBox.Location = new Point(6, 27);
+            ElementCheckedListBox.Location = new Point(5, 20);
+            ElementCheckedListBox.Margin = new Padding(3, 2, 3, 2);
             ElementCheckedListBox.Name = "ElementCheckedListBox";
-            ElementCheckedListBox.Size = new Size(191, 70);
+            ElementCheckedListBox.Size = new Size(168, 58);
             ElementCheckedListBox.TabIndex = 0;
             ElementCheckedListBox.SelectedIndexChanged += ElementCheckedListBox_SelectedIndexChanged;
             // 
             // FilterForm
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(223, 211);
+            ClientSize = new Size(195, 158);
             Controls.Add(groupBox1);
             FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(3, 2, 3, 2);
             MaximizeBox = false;
             MinimizeBox = false;
             Name = "FilterForm";
